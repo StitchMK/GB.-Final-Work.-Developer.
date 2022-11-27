20e98b53bb0b7d0a0c06e8aea2204bd2ebbb44aa
Maria Schneider <maria.schneider.aug@gmail.com>
2022-11-27 11:33:12 +0100

    NoMoreThenThree-method added

--- Program.cs
+++ Program.cs
@@ -12,16 +12,56 @@
     return newArray;
 }
 
-void ShowArray(int[] array)
+/*
+void ShowArray(string[] array)
 {
-    for(int i = 0; i < array.Length; i++)
+    Console.Write("[");
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");   
+    }
     
-        Console.Write(array[i] + " ");   
+
+    Console.Write($"{array[array.Length - 1]}");   
+    
+
+    Console.Write("]");
+    Console.WriteLine();
+}
+*/
+
+void NoMoreThenThree(string[] array)
+{   
+    Console.Write("[");
+    for(int i = 0; i < array.Length - 1; i++)
+    {   
+        string n = array[i];
+        int l = n.Length;
+        
+        if(l <= 3)
+        {
+            Console.Write($"{n}, ");
+        }
+    }
+
+        
+    if(array[array.Length - 1].Length <= 3)
+    {
+        Console.Write($"{array[array.Length - 1]}");
+    }
+    
+
+    Console.Write("]");
     Console.WriteLine();
 }
 
 Console.Write("Input a number of elements: ");
 int size = Convert.ToInt32(Console.ReadLine());
 string[] myArray = CreateArray(size);
+Console.WriteLine("This is your array: ");
 ShowArray(myArray);
+Console.WriteLine("This is an array of elements whose string length is no longer than 3: ");
+NoMoreThenThree(myArray);
+
+
 
